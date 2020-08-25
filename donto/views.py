6857c56204a636c74ddb8a872ce8d3ce75671578
charrouf outmane <charroufothmane@gmail.com>
2020-08-25 20:38:50 +0100

    first commit

---
+++ donto/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+import django
+from django.conf import settings
+from django.core.mail import send_mail
+
+# Create your views here.
+def home(request):
+    return  render(request,'home.html',{})
+    
+def about(request):
+    return  render(request,'about.html',{})
+def service(request):
+    return  render(request,'service.html',{})
+def pricing(request):
+    return  render(request,'pricing.html',{})
+def blog(request):
+    return  render(request,'blog.html',{})
+
+def contact(request):
+
+    if request.method=="POST":
+        message_name=request.POST["message-name"]
+        message_email=request.POST["message-email"]
+        message =request.POST["message"]
+        return  render(request,'contact.html',{"message_name":message_name})
+        # send_mail('Subject here', 'Here is the message.', settings.EMAIL_HOST_USER,
+        #  ['elfarah61@gmail.com'], fail_silently=False)
+
+        # #send email
+        send_mail(
+           message_name , #subject
+           message ,#message
+           message_email ,#from email
+            ['osmanout96@gmail.com'],
+             fail_silently=False)
+    # #to email
+
+        
+    else:
+        
+
+        return  render(request,'contact.html',{})

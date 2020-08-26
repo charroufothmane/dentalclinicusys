31e9ff3814868be52d056cade255f0162731d7c3
charrouf outmane <charroufothmane@gmail.com>
2020-08-26 23:42:33 +0100

    make it better

--- dentist/settings.py
+++ dentist/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '5_khk&jcb^b4l%%!3rca=5j!4(=9f8saqtid9vv%$3ya&!jw-0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dentalclinicus.herokuapp.com']
 
 
 # Application definition
